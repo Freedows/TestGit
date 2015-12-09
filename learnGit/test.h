0d5138f8ecddb757ae1bce1b4a6e03a243f89efa
Freedow <353013742@qq.com>
2015-12-09 23:06:03 +0800

    init

---
+++ learnGit/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  learnGit
+//
+//  Created by Freedow on 15/12/9.
+//  Copyright (c) 2015年 Freedow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test : NSObject
+
+@end
---
+++ learnGit/test.m
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  learnGit
+//
+//  Created by Freedow on 15/12/9.
+//  Copyright (c) 2015年 Freedow. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
